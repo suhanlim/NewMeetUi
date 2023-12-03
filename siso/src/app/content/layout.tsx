1fb6701c6cbf6227245f28c4efb99eda5d666e2d
suhanlim <soohan200964@gmail.com>
2023-12-03 22:05:44 +0900

    feat: calendars page

---
+++ siso/src/app/content/layout.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "@/components/Navbar";
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <div className="">{children}</div>
+    </>
+  );
+}

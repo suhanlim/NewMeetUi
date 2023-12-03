81bcc37e835a27713f6bdd51b0f95581c390d534
suhanlim <soohan200964@gmail.com>
2023-12-04 05:29:10 +0900

    feat: Calendars page

--- siso/src/app/content/calendarlist/page.tsx
+++ siso/src/app/content/calendarlist/page.tsx
@@ -1,5 +1,13 @@
 import { Calendar } from "@/components/Calendar";
+import { SchedulePanel } from "@/components/SchedulePanel";
 
 export default function Page() {
-  return <Calendar />;
+  return (
+    <div className=" flex h-screen flex-col md:flex-row md:overflow-hidden gap-2">
+      <div className="w-full flex-none max-w-sm">
+        <SchedulePanel />
+      </div>
+      <Calendar />
+    </div>
+  );
 }
---
+++ siso/src/components/SchedulePanel.tsx
@@ -0,0 +1,51 @@
+export function SchedulePanel() {
+  return (
+    <div className="pt-5 h-full flex flex-col gap-2">
+      <ul role="list" className="space-y-3">
+        <li className="hover:bg-blue-400 font-bold text-white text-center overflow-hidden bg-blue-500 px-4 py-4 shadow sm:rounded-md sm:px-6">
+          나의 캘린더
+        </li>
+      </ul>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center" aria-hidden="true">
+          <div className="w-full border-t border-gray-300" />
+        </div>
+        <div className="relative flex justify-center">
+          <span className="bg-sky-50 px-3 text-base font-semibold leading-6 text-gray-900">
+            친구 캘린더 목록
+          </span>
+        </div>
+      </div>
+
+      <ul role="list" className="space-y-3">
+        <li className="hover:bg-green-300 font-bold text-white text-center overflow-hidden bg-green-400 px-4 py-4 shadow sm:rounded-md sm:px-6">
+          친구 1 캘린더
+        </li>
+        <li className="hover:bg-green-300 font-bold text-white text-center overflow-hidden bg-green-400 px-4 py-4 shadow sm:rounded-md sm:px-6">
+          친구 2 캘린더
+        </li>
+      </ul>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center" aria-hidden="true">
+          <div className="w-full border-t border-gray-300" />
+        </div>
+        <div className="relative flex justify-center">
+          <span className="bg-sky-50 px-3 text-base font-semibold leading-6 text-gray-900">
+            그룹 캘린더 목록
+          </span>
+        </div>
+      </div>
+      <ul role="list" className="space-y-3">
+        <li className="hover:bg-sky-700 font-bold text-white text-center overflow-hidden bg-sky-800 px-4 py-4 shadow sm:rounded-md sm:px-6">
+          그룹 1 캘린더
+        </li>
+        <li className=" hover:bg-sky-700 font-bold text-white text-center overflow-hidden bg-sky-800 px-4 py-4 shadow sm:rounded-md sm:px-6">
+          그룹 2 캘린더
+        </li>
+        <li className="hover:bg-sky-700 font-bold text-white text-center overflow-hidden bg-sky-800 px-4 py-4 shadow sm:rounded-md sm:px-6">
+          그룹 3 캘린더
+        </li>
+      </ul>
+    </div>
+  );
+}

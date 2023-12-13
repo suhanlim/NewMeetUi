f5f07f5ff3a5cac94bc55123239f6ab4a024c740
suhanlim <soohan200964@gmail.com>
2023-12-14 03:41:25 +0900

    CLN

--- siso/src/components/Calender/WeekView.tsx
+++ siso/src/components/Calender/WeekView.tsx
@@ -43,7 +43,7 @@ export function WeekView() {
         >
           {v}
           <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
-            {dayjs().day(vIndex)["$D"]}
+            {dayjs().day(vIndex).date()}
           </span>
         </button>
       );
@@ -75,7 +75,7 @@ export function WeekView() {
             className="sticky top-0 z-30 flex-none bg-white shadow ring-1 ring-black ring-opacity-5 sm:pr-8"
           >
             <div className="grid grid-cols-7 text-sm leading-6 text-gray-500 sm:hidden">
-              {buttonMap}
+              {buttonMap()}
             </div>
 
             <div className="-mr-px hidden grid-cols-7 divide-x divide-gray-100 border-r border-gray-100 text-sm leading-6 text-gray-500 sm:grid">

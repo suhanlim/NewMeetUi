8094d46411c2cfc705ac8e4e84871e6ab5c543f0
suhanlim <soohan200964@gmail.com>
2023-12-14 03:47:32 +0900

    CLN

--- siso/src/components/Calender/WeekView.tsx
+++ siso/src/components/Calender/WeekView.tsx
@@ -33,22 +33,7 @@ export function WeekView() {
       );
     });
   };
-  const buttonMap = () => {
-    dayShortArray.map((v, vIndex) => {
-      return (
-        <button
-          type="button"
-          className="flex flex-col items-center pb-3 pt-2"
-          key={vIndex}
-        >
-          {v}
-          <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
-            {dayjs().day(vIndex).date()}
-          </span>
-        </button>
-      );
-    });
-  };
+  const buttonMap = () => {};
   useEffect(() => {
     // Set the container scroll position based on the current time.
     const currentMinute = new Date().getHours() * 60;
@@ -75,7 +60,20 @@ export function WeekView() {
             className="sticky top-0 z-30 flex-none bg-white shadow ring-1 ring-black ring-opacity-5 sm:pr-8"
           >
             <div className="grid grid-cols-7 text-sm leading-6 text-gray-500 sm:hidden">
-              {buttonMap()}
+              {dayShortArray.map((v, vIndex) => {
+                return (
+                  <button
+                    type="button"
+                    className="flex flex-col items-center pb-3 pt-2"
+                    key={vIndex}
+                  >
+                    {v}
+                    <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
+                      {dayjs().day(vIndex).date()}
+                    </span>
+                  </button>
+                );
+              })}
             </div>
 
             <div className="-mr-px hidden grid-cols-7 divide-x divide-gray-100 border-r border-gray-100 text-sm leading-6 text-gray-500 sm:grid">
